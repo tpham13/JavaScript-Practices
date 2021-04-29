3cdefbc3646957c34cdc6a9dad7dbe3fad64e39c
tpham13 <tpham0123@gmail.com>
2021-04-28 22:00:15 -0700

    linear time complexity result

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("testing...")
+function sumNumbers(numbers=[1,3,4,5]) {
+    let result = 0; 
+    // for of loop
+
+    for(const number of numbers){
+       result += number;
+    }
+    return result
+}
+// T = 1 + 1 + 1 + n = 3 + n = 3 + 1*n
+// T = 1*n
+// T = n => 0(n) => Linear Time Complexity 
+console.log(sumNumbers([1,2,3,4,6,10]))
