20f004edb76539a358c267ab241c4cc9f30b1390
tpham13 <tpham0123@gmail.com>
2021-04-28 22:28:06 -0700

    using reduce

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-function sumNumbers(numbers=[1,3,4,5]) {
-    let result = 0; 
+function sumNumbers(numbers) {
+    let result = 0; //run 1
     // for of loop
 
-    for(const number of numbers){
-       result += number;
+    for(const number of numbers){ //run 1
+       result += number; //run as many time as n
     }
-    return result
+    return result //run 1 
 }
 // T = 1 + 1 + 1 + n = 3 + n = 3 + 1*n
 // T = 1*n
 // T = n => 0(n) => Linear Time Complexity 
+console.log(sumNumbers([1,2,3,4,6,10]))
+
+
+// Can we do better? 
+
+function sum(num) {
+    return num.reduce((result, curNum) => sum + curNum, 0) //reduce will always gives you linear time complexity
+}
+
 console.log(sumNumbers([1,2,3,4,6,10]))
