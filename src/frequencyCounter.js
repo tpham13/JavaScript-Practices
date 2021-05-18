ca63dcb44e03ec848b09596cbfa7cce53e321b62
tpham13 <tpham0123@gmail.com>
2021-05-17 22:49:01 -0700

    Frequency, O(n)

--- src/frequencyCounter.js
+++ src/frequencyCounter.js
@@ -35,3 +35,31 @@ function same(arr1, arr2){
 }
 
 console.log(same([1,2,3,2], [9,1,4,4])) //true
+
+// REFACTOR
+function same(arr1, arr2) {
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    for(let value of arr1) {
+        frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1
+    }
+    for(let value of arr2) {
+        frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false;
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false;
+        }
+    }
+    return true
+}
+same([1,2,3,2], [9,1,4,4])
