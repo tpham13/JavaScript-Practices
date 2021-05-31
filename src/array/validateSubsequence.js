cca900acaa971461c7f32f0128373060b839cd18
tpham13 <tpham0123@gmail.com>
2021-05-30 20:44:04 -0700

    Solution 2

--- src/array/validateSubsequence.js
+++ src/array/validateSubsequence.js
@@ -17,3 +17,12 @@
      }
     return sequenceIndex === sequence.length;
  }
+// solution 2: 
+function isValidSubsequence(array,sequence) {
+    let sequenceIndex = 0; 
+    for (const value of array) {
+        if (sequenceIndex === sequence.length) break;
+        if (sequence[sequenceIndex] === value) sequenceIndex++;
+    }
+    return sequenceIndex === sequence.length;
+}
