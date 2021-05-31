38df3d0a122467928d4e1ba021facb645fdd804b
tpham13 <tpham0123@gmail.com>
2021-05-30 21:23:33 -0700

    Add solution notes

--- src/array/validateSubsequence.js
+++ src/array/validateSubsequence.js
@@ -11,8 +11,16 @@ a subsequence is a sequence that can be derived from another sequence
 by deleting some or no elements without changing the order of the 
 remaining elements. */
 
+/* traverse both array. 
+How do we traverse the array? Keep in mind that the order of the element matter 
+because it's a "subsequence"
+Create a pointer for each array (array & sequence) then Iterate through 
+the 1st element in the sequence and array to see if the sequence 1st element matches the 1st element 
+in the array. If it is not, then you move the pointer to the next element in the array. 
+If you find a match, then move the pointer in the sequence to the next element 
+*/
 //  solution 1: O(n) Time | O(1) Space
- function isValidSubsequence(array, sequence) {
+ function isValidSubsequence(array, sequence) { 
      let arrayIndex = 0; 
      let sequenceIndex = 0; 
      while (arrayIndex < array.length && sequenceIndex < sequence.length) {
@@ -22,7 +30,7 @@ remaining elements. */
      }
     return sequenceIndex === sequence.length;
  }
-// solution 2: 
+// solution 2: O(n) Time | O(1) Space
 function isValidSubsequence(array,sequence) {
     let sequenceIndex = 0; 
     for (const value of array) {
