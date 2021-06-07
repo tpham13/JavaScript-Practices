6a49fed430c0738e618841d0aa53b001fb19428d
tpham13 <tpham0123@gmail.com>
2021-06-07 13:58:11 -0700

    Added solution 1

---
+++ src/frequencyCounter/frequencyCounter-sameFrequency.js
@@ -0,0 +1,40 @@
+/* Write a function called sameFrequency. Given two positive integers, 
+find out if the two numbers have the same frequency of digits. 
+Your solution MUST have the following complexities: 
+Time: O(n) 
+sample input: 
+sameFrequency(182, 183) //true
+sameFrequency(34, 14) //false
+sameFrequency(3589578, 5879385) //true 
+sameFrequency(22, 222)// false */
+
+
+
+function sameFrequency(first, second){
+    // good luck. Add any arguments you deem necessary.
+    let num1 = first.toString();
+    let num2 = second.toString();
+    
+    if(num1.length !== num2.length) return false;
+    
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {}; 
+    
+    for (let value of num1){
+        frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1
+    }
+    for (let value of num2){
+        frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1
+    }
+    
+    for (let key in frequencyCounter1){
+        
+         if(!(key in frequencyCounter2)){
+              return false
+          }
+        if(frequencyCounter2[key] !==frequencyCounter1[key]){
+            return false;
+        }
+    }
+    return true
+  }
