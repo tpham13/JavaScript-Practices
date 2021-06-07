8485305e4a4c8afa619d990af5d785c3c7780349
tpham13 <tpham0123@gmail.com>
2021-06-07 15:07:45 -0700

    Added solution 2

--- src/frequencyCounter/frequencyCounter-sameFrequency.js
+++ src/frequencyCounter/frequencyCounter-sameFrequency.js
@@ -38,3 +38,36 @@ function sameFrequency(first, second){
     }
     return true
   }
+
+ 
+// Using for loops
+  function sameFrequency(first, second){
+    // good luck. Add any arguments you deem necessary.
+    let num1 = first.toString();
+    let num2 = second.toString();
+    
+    if(num1.length !== num2.length) return false;
+    
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {}; 
+    
+    for(let i = 0; i < strNum1.length; i++){
+        countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+    }
+    for(let j = 0; j < strNum1.length; j++){
+        countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+    }
+    
+    for (let key in frequencyCounter1){
+        
+         if(!(key in frequencyCounter2)){
+              return false
+          }
+        if(frequencyCounter2[key] !==frequencyCounter1[key]){
+            return false;
+        }
+    }
+    return true
+  }
+ 
+ 
