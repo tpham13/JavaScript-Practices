5a62377aa155bb9918ffbbdbb158338a115aef47
tpham13 <tpham0123@gmail.com>
2021-06-10 12:03:36 -0700

    Solution 1: brute force

--- src/pointer/trappingRainWaterLeetCodeHard.js
+++ src/pointer/trappingRainWaterLeetCodeHard.js
@@ -12,36 +12,32 @@ Output: 9
 https://leetcode.com/problems/trapping-rain-water/
 */
 
-/* thought on solution: 
-- edge cases: what if there's no wall height, then return 0. 
-What if the wall height is all the same height and there's no space in between?, then return 0. 
- 
-- the max height of the water that can be filled cannot exceed the 
-max height of the wall */
-function trap(height){
-    // edge case:
-    if(!height.length) return 0;
-    // initialize variables sum, wallHeight, maxHeight, heightIndex
-    let sum = 0; 
-    // wall height is the current height of of the wall
-    let wallHeight = 0;
-    let maxHeight = height[0];
-    // we will set the heightIndex to the index of the max Height of the array
-    let heightIndex = 0;
-    // find the index of the highest height in heights 
-    for (let i = 0; i < height.length; i++){
-        // calculate the highest elevation of the array
-        if (heigh[i] > maxHeight) {
-            maxHeight = height[i];
-            heightIndex = i
+// solution 1: brute force approach 
+function water(height){
+    total = 0; 
+    //for every element of the array, except the first and last element
+    for (let i = 1; i < height.length - 1; i++){
+        //find the max left height
+        let maxLeft = height[i];
+        for (let j = 0; j < i; j++ ){
+            maxLeft = Math.max(maxLeft, height[j])
         }
+        //find the max right height
+        let maxRight = height[i]
+        for (let j = i + 1; j < height.length; j++){
+            maxRight = Math.max(maxRight, height[j])
+        }
+        //update total water value 
+        total += Math.min(maxLeft, maxRight) - height[i];
+        console.log(maxLeft, height[i], maxRight)
+
     }
-    // loop over heights until the highest index 
-    // if current height is taller than wallHeightm wall height = current height
-    // at each index, find the difference between the current height and the wall height
-    
-    return sum
+    return total;
+
 }
+    
+
+water([0,1,0,2,1,0,1,3,2,1,2,1])
 //----------------------------------------------------------------
 
 //solution 2: using pointer
